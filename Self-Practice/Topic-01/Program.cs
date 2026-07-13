29669ba51611a92baa9e1800e5faf45f3afbcce8
Harsha Bhagya Prageeth Wijerathne <mhbpwijerathne@students.nsbm.ac.lk>
2026-07-14 00:35:26 +0530

    Add files via upload

---
+++ Self-Practice/Topic-01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopApp
+{
+    class Fruit
+    {
+        public string name = "Apple";
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Fruit myFruit = new Fruit();
+            Console.WriteLine(myFruit.name);
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
