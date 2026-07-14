7479023c83a27f4170a43b66ba5f644bb993140c
Harsha Bhagya Prageeth Wijerathne <mhbpwijerathne@students.nsbm.ac.lk>
2026-07-14 07:08:11 +0530

    Add files via upload

---
+++ Self-Practice/Topic-01/laptopApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopApp
+{
+    class Laptop
+    {
+        public string brand = "HP";
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Laptop myLaptop = new Laptop();
+            Console.WriteLine(myLaptop.brand);
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
