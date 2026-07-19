4d54199cc954adca654638d3dc6960c6d51f701d
Harsha Bhagya Prageeth Wijerathne <mhbpwijerathne@students.nsbm.ac.lk>
2026-07-19 09:50:54 +0530

    Add files via upload

---
+++ Lectures/Lecture-01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        NameRepeater repeater = new NameRepeater();
+        Console.Write("How many times should I print?");
+        int count = Convert.ToInt32(Console.ReadLine());
+        repeater.PrintName(count);
+    }
+}
+class NameRepeater
+{
+    public string myName = "Harsha Wijerathne";
+    public void PrintName(int times)
+    {
+        for (int i = 1; i <= times; i++)
+        {
+            Console.WriteLine(myName);
+        }
+
+    }
+}
