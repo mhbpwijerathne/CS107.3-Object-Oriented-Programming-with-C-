eeb923b2fea74d972caecc93624765d92db662af
Harsha Bhagya Prageeth Wijerathne <mhbpwijerathne@students.nsbm.ac.lk>
2026-07-20 22:40:04 +0530

    Add files via upload

---
+++ Self-Practice/Topic-01/student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Student
+{
+    public string name;
+    public int age;
+    public void DisplayDetails()
+    {
+        Console.WriteLine($"Student Name:{name},Age:{age}");
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Student student1 = new Student();
+        student1.name = "Harsha wijerathne";
+        student1.age = 23;
+        student1.DisplayDetails();
+
+    }
+}
