71d869503c33e056d9d0accc7c600f188e410ab1
Harsha Bhagya Prageeth Wijerathne <mhbpwijerathne@students.nsbm.ac.lk>
2026-07-21 21:52:39 +0530

    Add files via upload

---
+++ Self-Practice/Topic-01/car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class car
+{
+    public string model;
+    public string color;
+    public int year;
+
+    public void fullThrottle()
+    {
+        Console.WriteLine("The car is going full throttle!");
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        car Ford = new car();
+        Ford.model = "Mustang";
+        Ford.color = "red";
+        Ford.year = 1969;
+
+        car Opel = new car();
+        Opel.model = "Astra";
+        Opel.color = "blue";
+        Opel.year = 2005;
+
+        Console.WriteLine($"{Ford.model} , {Ford.color}, {Ford.year}");
+
+    }
+
+}
